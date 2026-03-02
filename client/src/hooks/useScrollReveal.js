985e7dfbcca6ba11eb2b4a513e846a28619debfe
vignesh <cvickey484@gmail.com>
2026-03-02 14:50:22 +0530

    change1

--- client/src/hooks/useScrollReveal.js
+++ client/src/hooks/useScrollReveal.js
@@ -5,7 +5,7 @@ export default function useScrollReveal() {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    entry.target.classList.add('revealed');
+                    entry.target.classList.add('visible');
                 }
             });
         }, { threshold: 0.1, rootMargin: '0px 0px -40px 0px' });
